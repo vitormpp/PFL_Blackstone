b83959650de29a8810bb7bd389f4be3c7d64ccb1
Vitor Pires <vitormanuelpp@gmail.com>
2024-12-29 03:53:44 +0000

    feat: create board

--- src/board.pl
+++ src/board.pl
@@ -18,3 +18,42 @@ cycle_aux(N, L1, Acc, R):-
     N1 is N - 1,
     append(L1, Acc, Acc1),
     cycle_aux(N1, L1, Acc1, R).
+
+% get_board(+Size, -Board)
+get_board(Size, Board):-
+    create_aux(0, Size, Board).
+
+% create_aux(+Index, +Size, -Board)
+create_aux(Index, Size, []) :-
+    Index >= Size, !.
+
+create_aux(Index, Size, [Element|Sublist]) :-
+    Index >= 0,
+    Index < Size,
+    create_row(Index, Size, Element),
+    Index1 is Index + 1,
+    create_aux(Index1, Size, Sublist).
+
+% create_row(+Index, +Size, -Row)
+create_row(0, Size, Row):-
+    N is Size // 2 - 1,!,
+    cycle(N, [' ','r'], L),
+    append(L, [' ',' '], Row).
+
+create_row(Index, Size, Row):-
+    Index is Size - 1,
+    N is Size // 2 - 1,!,
+    cycle(N, ['r',' '], L),
+    append([' ',' '], L, Row).
+
+create_row(Index, Size, Row):-
+    Index mod 2 =:= 0,
+    N is Size - 1,
+    create_list(' ', N, R),
+    append(['b'], R, Row).
+
+create_row(Index, Size, Row):-
+    Index mod 2 =:= 1,
+    N is Size - 1,
+    create_list(' ', N, R),
+    append(R, ['b'], Row).

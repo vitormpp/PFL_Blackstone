434940f4ca205879ae3c8ef494006be5423d4814
Vitor Pires <vitormanuelpp@gmail.com>
2024-12-29 03:53:08 +0000

    feat: create list

---
+++ src/board.pl
@@ -0,0 +1,6 @@
+% create_list(+Element, +Size, -List)
+create_list(_, 0, []).
+create_list(Element, Size, [Element|Sublist]) :-
+    Size > 0,
+    Size1 is Size - 1,
+    create_list(Element, Size1, Sublist).
